1ff9dca85b63bb48903cd8c66df91bd9843a6d22
NTmatter <NTmatter@5528d09e-c771-11dd-8ca8-e3ff79f713b6>
2008-12-22 13:38:42 +0000

    Update to use TestNG with Surefire plugin

--- src/test/java/com/blastradius/qa/tseng/AppTest.java
+++ src/test/java/com/blastradius/qa/tseng/AppTest.java
@@ -1,38 +1,18 @@
-package com.blastradius.qa.tseng;
+package org.blastradius.qa;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testApp() {
+        assertTrue(true);
     }
 }
