db4100708106a1027a0f62472e19849373dda0db
NTmatter <NTmatter@5528d09e-c771-11dd-8ca8-e3ff79f713b6>
2008-12-22 12:54:45 +0000

    Initial import of Maven project

---
+++ src/main/java/com/blastradius/qa/tseng/App.java
@@ -0,0 +1,13 @@
+package com.blastradius.qa.tseng;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
